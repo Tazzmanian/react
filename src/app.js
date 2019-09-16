32dd70e889469666a50c302d0184cdd0c3bbd4c7
Tazzmanian <tazzmanian@abv.bg>
2019-09-16 14:17:58 +0300

    event and attributes

--- src/app.js
+++ src/app.js
@@ -44,5 +44,22 @@ const template = (
         </ol>
     </div>
 );
+
+
+let count = 0;
+const addOne = () => {
+    count++;
+    console.log(count)
+}
+const removeOne = () => count--;
+const template2 = (
+    <div>
+        <h1>Count: {count}</h1>
+        <button id="plus" className="button" onClick={addOne}>+1</button>
+        <button id="minus" className="button" onClick={removeOne}>-1</button>
+    </div>
+);
+
+
 const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
