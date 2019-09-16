9f847f9f99becdf03f43b3577f90578e51c98f19
Tazzmanian <tazzmanian@abv.bg>
2019-09-16 11:50:47 +0300

    Explorer JSX

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ console.log('App.js is running!')
 
 
 // JSX - JavaScript XML
-var template = <p>This is something new app.js!</p>
+var template = (
+    <div>
+        <h1>LOL</h1>
+        <p>This is something new app.js!</p>
+        <ol>
+            <li>One</li>
+            <li>Two</li>
+        </ol>
+    </div>
+);
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
