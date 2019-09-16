96baa779fdc5abc4c1544129498341934f395bee
Tazzmanian <tazzmanian@abv.bg>
2019-09-16 11:37:13 +0300

    add babel

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ console.log('App.js is running!')
 
 
 // JSX - JavaScript XML
-var template = React.createElement('h1', {id: 'someid'}, 'Something new');
+var template = <p>This is something new app.js!</p>
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
