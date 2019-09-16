c737e51c7cb9cc16e270ff18b7d98aa84f46730e
Tazzmanian <tazzmanian@abv.bg>
2019-09-16 17:38:20 +0300

    components

--- src/app.js
+++ src/app.js
@@ -1,97 +1,67 @@
-console.log('App.js is running!')
-
-
-const name = 'tst'
-const list = {
-    one: 'One',
-    two: 'Two'
-}
-
-function three(num) {
-    if( num === 3) {
-        return 'Three';
-    }
-    return 'Unknown';
-}
-
-function four(num) {
-    if( num === 4) {
-        return <li>Four</li>;
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        );
     }
 }
-// JSX - JavaScript XML
-const template = (
-    <div>
-        <h1>{name.toUpperCase()}</h1>
-        <p>This is something new app.js!</p>
-        <ol>
-            <li>{list.one}</li>
-            <li>{list.two}</li>
-            <li>{three(3)}</li>
-            <li>{three()}</li>
-            {
-                four(4)
-            }
-            {
-                four()
-            }
-            {
-                true ? <li>Five</li> : ''
-            }
-            {
-                (6 === 6) && <li>Six</li>
-            }
-        </ol>
-    </div>
-);
 
+class Header extends React.Component {
 
-let count = 0;
-const addOne = () => {
-    count++;
-    console.log(count);
-    render();
-}
-const minusOne = () => {
-    count--;
-    render();
-}
-
-const options = [];
-
-const getOptions = () => {
-    return options.map(x => <li key={x}>{x}</li>);
-}
-
-const onFormSubmit = (event) => {
-    event.preventDefault();
-    const option = event.target.elements.option.value;
-    if (!option) {
-        return;
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put ....</h2>
+            </div>
+        );
     }
-    options.push(option);
-    event.target.elements.option.value = '';
-    render();
 }
 
-const render = () => {
-    const template2 = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button id="plus" className="button" onClick={addOne}>+1</button>
-            <button id="minus" className="button" onClick={minusOne}>-1</button>
-            <ol>
-                {getOptions()}
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option"/>
-                <button>Add Option</button>
-            </form>
-            <p>{options.length}</p>
-        </div>
-    );
-    const appRoot = document.getElementById('app');
-    ReactDOM.render(template2, appRoot);
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        );
+    }
 }
 
-render();
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>X</p>
+            </div>
+        )
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Options:</p>
+                <Option />
+            </div>
+        );
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Option Component</p>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
