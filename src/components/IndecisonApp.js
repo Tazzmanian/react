6009b5543bae81219eb64ef8c303f0ac5b1bc821
Tazzmanian <tazzmanian@abv.bg>
2019-09-18 16:35:36 +0300

    style the widget message

--- src/components/IndecisonApp.js
+++ src/components/IndecisonApp.js
@@ -84,14 +84,16 @@ export default class IndecisionApp extends React.Component {
                         hasOptions={this.state.options.length > 0}
                         handlePick={this.handlePick}
                     />
-                    <Options
-                        options={this.state.options}
-                        handleDeleteOptions={this.handleDeleteOptions}
-                        handleDeleteOption={this.handleDeleteOption}
-                    />
-                    <AddOption
-                        handleAddOptions={this.handleAddOptions}
-                    />
+                    <div className="widget">
+                        <Options
+                            options={this.state.options}
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteOption={this.handleDeleteOption}
+                        />
+                        <AddOption
+                            handleAddOptions={this.handleAddOptions}
+                        />
+                    </div>
                 </div>
                 <OptionModal
                     selectedOption={this.state.selectedOption}
--- src/components/Options.js
+++ src/components/Options.js
@@ -3,11 +3,14 @@ import Option from './Option';
 
 const Options = (props) => (
         <div>
-            <button
-                onClick={props.handleDeleteOptions}
-                className="button button--link"
-            >Remove all</button>
-            <p>Options ({props.options.length}):</p>
+            <div className="widget-header">
+                <h3 className="widget-header__title">Your Options</h3>
+                <button
+                    onClick={props.handleDeleteOptions}
+                    className="button button--link"
+                >Remove all</button>
+            </div>
+            {props.options.length === 0 && <div className="widget-body__message">Please add an option to get started!</div>}
             {
                 props.options.map(x => (
                     <Option
