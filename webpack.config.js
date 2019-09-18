29c5a45f5e5d9a8d578d2541727bb66e51ee0c69
Tazzmanian <tazzmanian@abv.bg>
2019-09-18 14:26:45 +0300

    config styling

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,17 @@ module.exports = {
                 loader: 'babel-loader',
                 test: /\.js$/,
                 exclude: /node_modules/
-            }
+            },
+            {
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+                test: /\.scss$/,
+                exclude: /node_modules/
+            },
+
         ]
     },
     devtool: 'cheap-module-eval-source-map',
