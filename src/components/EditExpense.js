3404500690e4f100e22455031f3c6786647d898a
Tazzmanian <tazzmanian@abv.bg>
2019-09-19 13:05:13 +0300

    add param to router

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        Edit page
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log(props)
+    return (
+        <div>
+            Edit page: param = {props.match.params.id}
+        </div>
+    );
+};
 
 export default EditExpensePage;
