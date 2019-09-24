ad2b1b60f15e352f1aaa45a27380b5c06f8a8356
Tazzmanian <tazzmanian@abv.bg>
2019-09-24 11:27:20 +0300

    scroll view

--- rnCourse/src/components/ListItems/ListItems.tsx
+++ rnCourse/src/components/ListItems/ListItems.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { StyleSheet, ScrollView } from "react-native";
 import ListItem from "../ListItem/ListItem";
 
 const ListItems = (props) => {
@@ -14,9 +14,9 @@ const ListItems = (props) => {
             />
         );
     return (
-        <View style={styles.listContainer}>
+        <ScrollView style={styles.listContainer}>
             {placesOutput}
-        </View>
+        </ScrollView>
     );
 }
 
