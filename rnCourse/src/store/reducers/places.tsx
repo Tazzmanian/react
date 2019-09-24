6a7d43860b20fc76ab1d1ffd50a1c768a0773894
Tazzmanian <tazzmanian@abv.bg>
2019-09-24 15:19:39 +0300

    add redux & reducer

---
+++ rnCourse/src/store/reducers/places.tsx
@@ -0,0 +1,13 @@
+const initialState = {
+    places: [],
+    selectedPlace: null
+};
+
+const reducer = (state = initialState, action) => {
+
+    switch (action) {
+        default: ''
+    }
+}
+
+export default reducer;
